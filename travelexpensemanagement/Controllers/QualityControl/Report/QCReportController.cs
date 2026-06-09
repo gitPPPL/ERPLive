20b6fe24898c53d87a4d8ca5f1c854567b0a70a6
sumeshbiswas <60379190+sumeshbiswas@users.noreply.github.com>
2026-06-09 13:41:33 +0530

    Item Market and QC Master Page Done

---
+++ travelexpensemanagement/Controllers/QualityControl/Report/QCReportController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace travelexpensemanagement.Controllers.QualityControl.Report
+{
+    public class QCReportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/QualityControl/Report/QCReport/Index.cshtml");
+        }
+    }
+}
