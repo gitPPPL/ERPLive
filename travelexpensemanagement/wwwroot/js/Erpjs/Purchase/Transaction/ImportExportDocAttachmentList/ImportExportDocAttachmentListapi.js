b08d0256a5337dbd25f2ad090be07eb36af89eca
Sarthak <erp.lead@pashupatigrp.com>
2026-08-07 15:47:41 +0530

    Changes

--- travelexpensemanagement/wwwroot/js/Erpjs/Purchase/Transaction/ImportExportDocAttachmentList/ImportExportDocAttachmentListapi.js
+++ travelexpensemanagement/wwwroot/js/Erpjs/Purchase/Transaction/ImportExportDocAttachmentList/ImportExportDocAttachmentListapi.js
@@ -50,159 +50,132 @@ function formatDate(date) {
     return `${day}/${month}/${year}`;
 }
 
-const tableHeaders = [
-    "Code",
-    "Sauda No",
-    "Sauda Date",
-    "Inv No",
-    "Inv Date",
-    "B E No.",
-    "Exim No",
-    "Exim Date",
-    "Party Name",
-    "Party Code",
-    "Chk",
-    "P I Copy",
-    "B L Copy",
-    "B E Copy",
-    "L C Copy",
-    "INV Copy",
-    "D P Copy",
-    "SBLC Copy",
-    "Oth Copy1",
-    "Oth Copy2",
-    "Oth Copy3",
-    "Oth Copy4",
-    "Oth Copy5",
-    "Oth Copy6",
-    "Oth Copy7"
+const tableHeadersExport = [
+    { text: "Code", className: "hidden-col" },
+    { text: "Inv No" },
+    { text: "Inv Date" },
+    { text: "Exim No" },
+    { text: "Exim Date" },
+    { text: "Party Name" },
+    { text: "Party Code" },
+    { text: "S B No." },
+    { text: "Chk" },
+    { text: "S B Copy" },
+    { text: "B L Copy" },
+    { text: "BRC Copy" },
+    { text: "Oth Copy1" },
+    { text: "Oth Copy2" },
+    { text: "Oth Copy3" },
+    { text: "Oth Copy4" },
+    { text: "Oth Copy5" },
+    { text: "Oth Copy6" },
+    { text: "Oth Copy7" }
+];
+
+const tableHeadersForImport = [
+    { text: "Code", className: "hidden-col" },
+    { text: "Sauda No" },
+    { text: "Sauda Date" },
+    { text: "Exim No" },
+    { text: "Exim Date" },
+    { text: "Party Name" },
+    { text: "Party Code" },
+    { text: "B E No." },
+    { text: "Chk" },
+    { text: "P I Copy" },
+    { text: "B L Copy" },
+    { text: "B E Copy" },
+    { text: "L C Copy" },
+    { text: "INV Copy" },
+    { text: "D P Copy" },
+    { text: "SBLC Copy" },
+    { text: "Oth Copy1" },
+    { text: "Oth Copy2" }
 ];
 
 function createTableHeader() {
-    let headerHtml = "<tr>";
+    let v_type = $('#ddltype').val();
+    let html = "<tr>";
 
-    tableHeaders.forEach((header, index) => {
-        if (index === 0) {
-            headerHtml += `<th class="hidden-col">${header}</th>`;
-        } else {
-            headerHtml += `<th>${header}</th>`;
-        }
-    });
+    if (v_type == "Import") {
+        tableHeadersForImport.forEach(h => {
+            html += `<th class="${h.className || ''}">${h.text}</th>`;
+        });
+    }
+    else {
+        tableHeadersExport.forEach(h => {
+            html += `<th class="${h.className || ''}">${h.text}</th>`;
+        });
+    }
 
-    headerHtml += "</tr>";
+    html += "</tr>";
 
-    $("#tblImportExportDocAttachmentList thead").html(headerHtml);
+    $("#tblImportExportDocAttachmentList thead").html(html);
 }
 
 function addItemRecordRow(item = null) {
+    let v_type = $('#ddltype').val();
 
     let tbody = $('#tblImportExportDocAttachmentList tbody');
-
+    let newRow = '';
     // Count only actual data rows
     let rowCount = tbody.find('tr[id^="row"]').length + 1;
 
-    let newRow = `
+    if (v_type == "Import") {
+         newRow = `
         <tr id="row${rowCount}" class="no-border-input">
 
             <td class="hidden-col">  <input type="hidden" id="TxtCode${rowCount}" value="${item?.code ?? ""}" /> </td>
-            <td style="display: none;"> <input type="text" id="TxtSaudaNo${rowCount}" class="erppagetable-control"  value="${item?.saudA_NO ?? ""}" /> </td>
+            <td > <input type="text" id="TxtSaudaNo${rowCount}" class="erppagetable-control"  value="${item?.saudA_NO ?? ""}" /> </td>
             <td> <input type="text" id="TxtSaudaDate${rowCount}"  class="erppagetable-control" value="${item?.sauda_Date ?? ""}" />  </td>
-            <td> <input type="text" id="TxtInvNo${rowCount}" class="erppagetable-control"  value="${item?.v_NO ?? ""}" />  </td>
-            <td>  <input type="text"  id="TxtInvDate${rowCount}"  class="erppagetable-control" value="${item?.invDate ?? ""}" />  </td>
+            <td > <input type="text" id="TxtSaudaNo${rowCount}" class="erppagetable-control"  value="${item?.eximNo ?? ""}" /> </td>
+            <td> <input type="text" id="TxtSaudaDate${rowCount}"  class="erppagetable-control" value="${item?.eximDate ?? ""}" />  </td>
+            <td>  <input type="text"  id="TxtPartyName${rowCount}"  class="erppagetable-control"  value="${item?.partyName ?? ""}" />  </td>
+            <td> <input type="text"  id="TxtPartyCode${rowCount}"   class="erppagetable-control" value="${item?.partY_CODE ?? ""}" />  </td> 
             <td> <input type="text"  id="TxtBENo${rowCount}"  class="erppagetable-control"  value="${item?.bE_NO ?? ""}" /> </td>
+               <td class="text-center"> <input type="checkbox" id="Chk${rowCount}" /> </td>
+            <td>  <input type="text"  id="TxtPICopy${rowCount}"  class="erppagetable-control"  value="${item?.piCopy ?? ""}" />  </td>
+            <td>  <input type="text"  id="TxtBLCopy${rowCount}"  class="erppagetable-control"  value="${item?.blCopy ?? ""}" /> </td>
+            <td> <input type="text"  id="TxtBECopy${rowCount}" class="erppagetable-control"  value="${item?.beCopy ?? ""}" />  </td>
+            <td>  <input type="text" id="TxtLCCopy${rowCount}" class="erppagetable-control" value="${item?.lcCopy ?? ""}" />  </td>
+            <td>  <input type="text" id="TxtINVCopy${rowCount}"  class="erppagetable-control"  value="${item?.invCopy ?? ""}" />  </td>
+            <td> <input type="text"  id="TxtDPCopy${rowCount}"  class="erppagetable-control"  value="${item?.dpCopy ?? ""}" />   </td>
+            <td> <input type="text"  id="TxtSBLCCopy${rowCount}"  class="erppagetable-control"  value="${item?.sblcCopy ?? ""}" />  </td>
+            <td>  <input type="text" id="TxtOthCopy1${rowCount}"  class="erppagetable-control" value="${item?.othCopy1 ?? ""}" />  </td>
+            <td> <input type="text"  id="TxtOthCopy2${rowCount}"  class="erppagetable-control"  value="${item?.othCopy2 ?? ""}" /> </td>
+            </tr>
+    `;
+    }
+    else {
+         newRow = `
+        <tr id="row${rowCount}" class="no-border-input">
+
+            <td class="hidden-col">  <input type="hidden" id="TxtCode${rowCount}" value="${item?.code ?? ""}" /> </td>
+             <td> <input type="text" id="TxtInvNo${rowCount}" class="erppagetable-control"  value="${item?.saudA_NO ?? ""}" />  </td>
+            <td>  <input type="text"  id="TxtInvDate${rowCount}"  class="erppagetable-control" value="${item?.sauda_Date ?? ""}" />  </td>
+
             <td> <input type="text" id="TxtEximNo${rowCount}" class="erppagetable-control" value="${item?.eximNo ?? ""}" />  </td>
             <td> <input type="text"  id="TxtEximDate${rowCount}"  class="erppagetable-control"  value="${item?.eximDate ?? ""}" />  </td>
             <td>  <input type="text"  id="TxtPartyName${rowCount}"  class="erppagetable-control"  value="${item?.partyName ?? ""}" />  </td>
             <td> <input type="text"  id="TxtPartyCode${rowCount}"   class="erppagetable-control" value="${item?.partY_CODE ?? ""}" />  </td> 
+            <td> <input type="text"  id="TXTSBNO{rowCount}"   class="erppagetable-control" value="${item?.SBNO ?? ""}" />  </td> 
             <td class="text-center"> <input type="checkbox" id="Chk${rowCount}" /> </td>
-            <td>  <input type="text"  id="TxtPICopy${rowCount}"  class="erppagetable-control"  value="${item?.piCopy ?? ""}" />  </td>
-            <td>  <input type="text"  id="TxtBLCopy${rowCount}"  class="erppagetable-control"  value="${item?.blCopy ?? ""}" /> </td>
-            <td> <input type="text"  id="TxtBECopy${rowCount}" class="erppagetable-control"  value="${item?.beCopy ?? ""}" />  </td>
-
-            <!-- 14. L C Copy -->
-            <td>  <input type="text" id="TxtLCCopy${rowCount}" class="erppagetable-control" value="${item?.lcCopy ?? ""}" />  </td>
-
-            <!-- 15. INV Copy -->
-            <td>  <input type="text" id="TxtINVCopy${rowCount}"  class="erppagetable-control"  value="${item?.invCopy ?? ""}" />  </td>
-
-            <!-- 16. D P Copy -->
-            <td>
-                <input type="text"
-                       id="TxtDPCopy${rowCount}"
-                       class="erppagetable-control"
-                       value="${item?.dpCopy ?? ""}" />
-            </td>
-
-            <!-- 17. SBLC Copy -->
-            <td>
-                <input type="text"
-                       id="TxtSBLCCopy${rowCount}"
-                       class="erppagetable-control"
-                       value="${item?.sblcCopy ?? ""}" />
-            </td>
-
-            <!-- 18. Oth Copy1 -->
-            <td>
-                <input type="text"
-                       id="TxtOthCopy1${rowCount}"
-                       class="erppagetable-control"
-                       value="${item?.othCopy1 ?? ""}" />
-            </td>
-
-            <!-- 19. Oth Copy2 -->
-            <td>
-                <input type="text"
-                       id="TxtOthCopy2${rowCount}"
-                       class="erppagetable-control"
-                       value="${item?.othCopy2 ?? ""}" />
-            </td>
-
-            <!-- 20. Oth Copy3 -->
-            <td>
-                <input type="text"
-                       id="TxtOthCopy3${rowCount}"
-                       class="erppagetable-control"
-                       value="${item?.othCopy3 ?? ""}" />
-            </td>
-
-            <!-- 21. Oth Copy4 -->
-            <td>
-                <input type="text"
-                       id="TxtOthCopy4${rowCount}"
-                       class="erppagetable-control"
-                       value="${item?.othCopy4 ?? ""}" />
-            </td>
-
-            <!-- 22. Oth Copy5 -->
-            <td>
-                <input type="text"
-                       id="TxtOthCopy5${rowCount}"
-                       class="erppagetable-control"
-                       value="${item?.othCopy5 ?? ""}" />
-            </td>
-
-            <!-- 23. Oth Copy6 -->
-            <td>
-                <input type="text"
-                       id="TxtOthCopy6${rowCount}"
-                       class="erppagetable-control"
-                       value="${item?.othCopy6 ?? ""}" />
-            </td>
-
-            <!-- 24. Oth Copy7 -->
-            <td>
-                <input type="text"
-                       id="TxtOthCopy7${rowCount}"
-                       class="erppagetable-control"
-                       value="${item?.othCopy7 ?? ""}" />
-            </td>
-
+            <td> <input type="text"  id="TxtSBLCCopy${rowCount}"  class="erppagetable-control"  value="${item?.sbCopy ?? ""}" />  </td>
+            <td> <input type="text"  id="TxtSBLCCopy${rowCount}"  class="erppagetable-control"  value="${item?.BLCopy ?? ""}" />  </td>
+            <td> <input type="text"  id="TxtSBLCCopy${rowCount}"  class="erppagetable-control"  value="${item?.BRCCopy ?? ""}" />  </td>
+            <td>  <input type="text" id="TxtOthCopy1${rowCount}"  class="erppagetable-control" value="${item?.othCopy1 ?? ""}" />  </td>
+            <td> <input type="text"  id="TxtOthCopy2${rowCount}"  class="erppagetable-control"  value="${item?.othCopy2 ?? ""}" /> </td>
+            <td> <input type="text" id="TxtOthCopy3${rowCount}" class="erppagetable-control" value="${item?.othCopy3 ?? ""}" />  </td>
+            <td> <input type="text"  id="TxtOthCopy4${rowCount}"  class="erppagetable-control"  value="${item?.othCopy4 ?? ""}" /> </td>
+            <td> <input type="text"  id="TxtOthCopy5${rowCount}"  class="erppagetable-control" value="${item?.othCopy5 ?? ""}" /> </td>
+            <td> <input type="text" id="TxtOthCopy6${rowCount}" class="erppagetable-control" value="${item?.othCopy6 ?? ""}" />  </td>
+            <td>  <input type="text"  id="TxtOthCopy7${rowCount}" class="erppagetable-control" value="${item?.othCopy7 ?? ""}" /> </td>
         </tr>
     `;
+    }
 
     tbody.append(newRow);
 }
-
 
 async function Viewdata() {
 
@@ -226,7 +199,7 @@ async function Viewdata() {
             }
         });
 
-        console.log(res);
+        console.log("Table Data:", res);
 
         const tbody = $('#tblImportExportDocAttachmentList tbody');
 
--- travelexpensemanagement/wwwroot/js/Erpjs/Purchase/Transaction/ImportExportDocAttachmentList/ImportExportDocAttachmentListinit.js
+++ travelexpensemanagement/wwwroot/js/Erpjs/Purchase/Transaction/ImportExportDocAttachmentList/ImportExportDocAttachmentListinit.js
@@ -1,4 +1,8 @@
 ﻿$(document).ready(function () {
+
+    createTableHeader();
+
+
     var today = new Date();
     var currentDate = today.getFullYear() + '-' + String(today.getMonth() + 1).padStart(2, '0') + '-' + String(today.getDate()).padStart(2, '0');
     $('#Dtfrom').val(currentDate);
@@ -8,15 +12,13 @@
     $('#btnviewdata').on('click', async function () {
 
         $('#tblImportExportDocAttachmentList tbody').empty();
-
-
         // Call once when page loads
-        createTableHeader();
-
 
         Viewdata();
 
     });
 
-
+    $('#ddltype').on('change', function () {
+        createTableHeader();
+    });
 });
